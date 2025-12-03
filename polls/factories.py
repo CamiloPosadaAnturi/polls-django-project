d8ec4c0763a427db37616dda15482f1742698be5
Developer <developer@Developers-MacBook-Pro.local>
2025-12-03 13:49:35 -0500

    changes in template and add factory

--- polls/factories.py
+++ polls/factories.py
@@ -1,10 +1,28 @@
 import factory
+from factory.django import DjangoModelFactory
 from django.utils import timezone
-from polls.models import Question
+from polls.models import Question, Choice
 
-class QuestionFactory(factory.django.DjangoModelFactory):
+class QuestionFactory(DjangoModelFactory):
     class Meta:
         model = Question
 
     question_text = factory.Faker("sentence")
     pub_date = factory.LazyFunction(timezone.now)
+
+    @factory.post_generation
+    def choices(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for choice_text in extracted:
+                ChoiceFactory(question=self, choice_text=choice_text)
+
+class ChoiceFactory(DjangoModelFactory):
+    class Meta:
+        model = Choice
+
+    question = factory.SubFactory("polls.factories.QuestionFactory")
+    choice_text = factory.Faker("word")
+    votes = 0
